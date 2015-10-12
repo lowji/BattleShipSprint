0cfb9ecac882f834b2fe25c9827d99fab4b60a9a
iamqaz <jonathon.d.meyers@gmail.com>
2015-10-13 10:28:47 +1100

    Add main menu option from game

--- src/GameController.cs
+++ src/GameController.cs
@@ -21,7 +21,56 @@ using static HighScoreController;
 /// </summary>
 public static class GameController
 {
+	private static readonly string[][] _menuStructure = 
+	{
+		new string[] 
+		{
+			"MAIN MENU"
+		}
+	};
+	private const int MENU_TOP = 575;
+	private const int MENU_LEFT = 30;
+	private const int MENU_GAP = 0;
+	private const int BUTTON_WIDTH = 75;
+	private const int BUTTON_HEIGHT = 15;
+	private const int BUTTON_SEP = BUTTON_WIDTH + MENU_GAP;
+	private const int TEXT_OFFSET = 0;
+	private const int MAIN_MENU = 0;
+	private const int NAME_WIDTH = 3;
+	private const int SCORES_LEFT = 490;
+	private static readonly Color MENU_COLOR = SwinGame.RGBAColor(2, 167, 252, 255);
+	private static readonly Color HIGHLIGHT_COLOR = SwinGame.RGBAColor(1, 57, 86, 255);
 
+	private static void DrawButtons(int menu, int level, int xOffset)
+	{
+		int btnTop = 0;
+
+		btnTop = MENU_TOP - (MENU_GAP + BUTTON_HEIGHT) * level;
+		int i = 0;
+		for (i = 0; i <= _menuStructure[menu].Length - 1; i++) {
+			int btnLeft = 0;
+			btnLeft = MENU_LEFT + BUTTON_SEP * (i + xOffset);
+			SwinGame.DrawTextLines(_menuStructure[menu][i], MENU_COLOR, Color.Black, GameFont("Menu"), FontAlignment.AlignCenter, btnLeft + TEXT_OFFSET, btnTop + TEXT_OFFSET, BUTTON_WIDTH, BUTTON_HEIGHT);
+
+			if (SwinGame.MouseDown(MouseButton.LeftButton) & IsMouseOverMenu(i, level, xOffset)) {
+				SwinGame.DrawRectangle(HIGHLIGHT_COLOR, btnLeft, btnTop, BUTTON_WIDTH, BUTTON_HEIGHT);
+				//EndCurrentState ();
+				if (CurrentState == GameState.Discovering)
+					//EndCurrentState ();
+					SwitchState(GameState.EndingGame);
+			}
+		}
+	}
+
+	private static bool IsMouseOverMenu(int button, int level, int xOffset)
+	{
+		int btnTop = MENU_TOP - (MENU_GAP + BUTTON_HEIGHT) * level;
+		int btnLeft = MENU_LEFT + BUTTON_SEP * (button + xOffset);
+
+		return IsMouseInRectangle(btnLeft, btnTop, BUTTON_WIDTH, BUTTON_HEIGHT);
+	}
+		
+	//orig code:
 	private static BattleShipsGame _theGame;
 	private static Player _human;
 
@@ -283,6 +332,7 @@ public static class GameController
 		//Read incoming input events
 		SwinGame.ProcessEvents();
 
+
 		switch (CurrentState) {
 			case GameState.ViewingMainMenu:
 				HandleMainMenuInput();
@@ -319,6 +369,7 @@ public static class GameController
 	public static void DrawScreen()
 	{
 		DrawBackground();
+		DrawButtons (MAIN_MENU, 0, 0);
 
 		switch (CurrentState) {
 			case GameState.ViewingMainMenu:
